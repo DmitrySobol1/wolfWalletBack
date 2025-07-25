6de74cd9041f4bf8e2f9967282115f087c4070ab
DmitrySobol1 <saibleonetwo@gmail.com>
2025-07-25 16:54:49 +0300

    25jul 04

---
+++ StockLimitCron/texts.js
@@ -0,0 +1,18 @@
+export const TEXTS = {
+ 
+  ru: {
+    title: 'Операция на бирже прошла успешно',
+    text: 'операция на бирже прошла успешно',
+  },
+  en: {
+    title: 'The crypto stock operation was  successfully done',
+    text: 'successfully sent to another user ',
+  },
+  de: {
+    title: 'Das Börsengeschäft war ein Erfolg',
+    text: 'erfolgreich an einen anderen Benutzer gesendet ',
+  }
+  
+  
+
+};
